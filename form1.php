98592c574f4f42b625862560fb38bf01cc736e5b
Morksha Rathnayake <73585584+RasanjaleeRathnayake@users.noreply.github.com>
2020-11-04 21:05:35 +0530

    Add files via upload

--- form1.php
+++ form1.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Your address is: <?php echo $_POST["address"]; ?>
 
 </body>
 </html>
---
+++ index.php
@@ -0,0 +1,497 @@
+<!DOCTYPE html>
+<html>
+<body style="background-image: url('dc.png');width:120" HSPACE="100">
+<title>Brownie_world</title>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!--navigation bar-->
+<style>
+body {font-family: "Times New Roman", Georgia, Serif}
+h1, h2, h3, h4, h5, h6 {
+  font-family: "Times New Roman";
+  letter-spacing: 2px; 
+  color: #523A28;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: left;
+  width: 33.3%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+.container {
+  padding: 0 16px;
+}
+
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color:#F3E1C0;
+
+
+}
+
+body {margin:0;}
+
+.navbar {
+  overflow: hidden;
+  background-color:#523A28;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.navbar a {
+  float: left;
+  display: block;
+  color:#E4D4C8;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 18px;
+  font-family:Georgia;
+}
+
+.navbar a:hover {
+  background: #ddd;
+  color: rgb(48, 3, 3);
+}
+
+.main {
+  padding: 16px;
+  margin-top: 30px;
+  height: 1500px; 
+}
+
+a {
+  text-decoration: none;
+  font-size: 22px;
+  color: black;
+}
+<!--contactus-->
+* {
+      box-sizing: border-box;
+      }
+      html, body {
+      min-height: 100vh;
+      padding: 0;
+      margin: 0;
+      font-family: Roboto, Arial, sans-serif;
+      font-size: 14px; 
+      color:#171515 ;
+      }
+      input, textarea { 
+      outline: none;
+      }
+      body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 20px;
+      background: #5a7233;
+     
+      }
+     
+      form {
+      position: center;
+      width: 80%;
+      border-radius: 30px;
+      background: #E4B7A0;
+      }
+      
+      .circle {
+      position: absolute;
+      bottom: 80px;
+      left: -55px;
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      background: #E4B7A0;
+      }
+      .form-inner {
+      padding: 40px;
+      }
+      .form-inner input,
+      .form-inner textarea {
+      display: block;
+      width: 100%;
+      padding: 15px;
+      margin-bottom: 10px;
+      border: none;
+      border-radius: 20px;
+      background: #412516;
+      color:white;
+      }
+      .form-inner textarea {
+      resize: none;
+      }
+      button {
+      width: 100%;
+      padding: 10px;
+      margin-top: 20px;
+      border-radius: 20px;
+      border: none;
+      border-bottom: 4px solid #E8BD30;
+      background: #F8CF2C; 
+      font-size: 16px;
+      font-weight: 400;
+      color: black;
+      }
+      button:hover {
+      background: #E8BD30;
+      } 
+      @media (min-width: 568px) {
+      form {
+      width: 60%;
+      }
+      }
+      .center {
+     display: block;
+     margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+}
+
+<!--icon-->
+.icon-bar {
+  width: 100%;
+  background-color: #555;
+  overflow: auto;
+}
+
+.icon-bar a {
+  float: left;
+  width: 25%;
+  text-align: center;
+  padding: 12px 0;
+  transition: all 0.3s ease;
+  color: #F8CF2C;
+  font-size: 36px;
+}
+
+.icon-bar a:hover {
+  background-color: #935F4C;
+}
+
+.active {
+  background-color: #291211;
+}
+<!--button-->
+
+.button {
+  background-color: #F7E7CE; /* Green */
+  border:5px;
+  color: #7A3E26;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 10px 5px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  
+}
+
+.button1 {
+  background-color: #F7E7CE; 
+  color:#7A3E26; 
+  border: 5px solid #7A3E26;
+  width:250px;
+  height:50px;
+
+}
+
+.button1:hover {
+  background-color: #7A3E26;
+  color: #F7E7CE;
+  width:250px;
+  height:50px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.title {
+  color: grey;
+  font-size: 15px;
+}
+a {
+  text-decoration: none;
+  font-size: 10px;
+  color: black;
+}
+
+button:hover, a:hover {
+  opacity: 0.7;
+}
+     </style>
+    
+    </head>
+    <body>
+
+<div class="navbar">
+  <a href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#menu">menu</a>
+  <a href="#contact">Contact</a>
+</div>
+
+
+
+</style>
+<body>
+
+  
+<!-- the home -->
+<div class="aboutusstart" id="home">
+  <div class="home">
+   <img src="Brownie.png" width=1520px height=800px>
+  <body>
+  <br><br>
+  <!-- About Section -->
+  <div class="aboutusstart" id="about">
+    <div class="aboutusimage">
+      <img src="Brownie (1).png" alt="brownie" class="center">
+    </div>
+    <div class="aboutuspara">
+        <h1 class="abtus" class="underlined_text"ALIGN="center"class="underlined_text"style="font-size:20px"> About our Brownies World</h1><br>
+
+        <h5 class="abtus" ALIGN="center" style=font-size:15px >  Proudly present since 2008</h5>
+        <p class="abtusparae"ALIGN="center" style="font-size:18px ;">  The Brownies World Café is a well-known cafeteria which sells you various kinds of<br></br> Brownies.
+             This café was founded by the Miss.Morksha Rathnayake who is having a great ability to make various kinds of <br></br>
+             brownies of various flavours. The Brownies World is first started on 2008 and currently situated at
+              Golumadama junction , Galle Road, ,Rathmalana, Sri Lanka. <br></br>
+              And we happy to sat that THE BROWNIES WORLD is ready make you customized Brownies according 
+              to your needs.<br></br>
+              The ingredients are all healthy and no any allergies<br></br> or harms happen
+      </div>
+    <hr>
+   
+</head>
+<body>
+
+  
+  <!-- Menu -->
+  <div class="aboutmenu" id="menu">
+    <div class="menustart">
+      <h1  style="vertical-align:middle;margin:20px 30px"class="underlined_text">Our Menu</u></h1><br>
+      <!--menue 1-->
+      <h4 style="font-size:30px"><ul style="list-style-type:square">
+        <li>Chocolate brownies</li></h4> 
+      <img src="chocolate.jpg" ALIGN="left"HSPACE="40"class="text1" alt="one" width="370" height="460" style="border-radius:20px;border-width:100px; border:50px;border-style:solid; border-color:#201A1E; padding: 1em;"> 
+      <p style="letter-spacing:2px ;font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color:#493743;font-size:20px;border:20px;border-width: 40px; border-style:solid; border-color:#7A3E26; padding: 2em;">
+        <b>Brownies enriched with chocolate flavour plus cream which made adults as well</b><br></br>
+      <b>as kids extremely high in taste. we use all the ingredients in high quality.</b> <br></br></p>
+      <b><h3 style="color:#44110E;font-family:Garamond">CHOCOLATE BROWNIES CAN BE CUSTOMERIZED ACCORDING TO YOUR DESIGNS.</h3>
+      <ul style="color:#201A1E;font:15;font-family:Georgia, 'Times New Roman', Times, serif"><b>
+      </ul>
+      <h4>See the prices with sizes availbale</h4><button class="button button1" onClick="location.href='prices.html'">Prices</button>
+      
+ 
+    <br><br><br><br><br><br><br>
+   
+     <!--menue 2-->
+     <h4 style="font-size:30px"><ul style="list-style-type:square">
+      <li>Mint Flavoured Brownies</li></h4> 
+    <img src="mintbrownies.jpg" ALIGN="left"HSPACE="40"class="text1" alt="one" width="370" height="460" style="border-radius:20px;border-width:100px; border:50px;border-style:solid; border-color:#013A20; padding: 1em;"> 
+    <p style="letter-spacing:2px ;font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color:#493743;font-size:18px;border:20px;border-width: 40px; border-style:solid; border-color:#3F4122; padding: 2em;">
+      <b>These were delicious! Followed the best recipe exactly, and got good reviews all around from </b><br></br>
+      <b>family and friends and all the people who taste this kind. You can feel the heavenly feeling.  </b><br></br>
+    <b>As its taste is very rare you can also give it as a gift to your lovelies </b> <br></br></p>
+    <b><h3 style="color:#44110E;font-family:Garamond">MINT FLAVOURED BROWNIES CAN BE CUSTOMERIZED ACCORDING TO YOUR DESIGNS.</h3>
+    <ul style="color:#201A1E;font:15;font-family:Georgia, 'Times New Roman', Times, serif"><b>
+    </ul>
+   <h4>See the prices with sizes availbale</h4><button class="button button1"  onClick="location.href='prices.html'">Prices</button>
+  </img>
+    <br><br><br><br><br><br>
+  
+   <!--menue 3-->
+   <h4 style="font-size:30px"><ul style="list-style-type:square">
+    <li>Blackberry Cheese Cake Brownies</li></h4> 
+  <img src="blackberrycheesecakebrownies.jpg" ALIGN="left"HSPACE="40"class="text1" alt="one" width="370" height="460" style="border-radius:20px;border-width:100px; border:50px;border-style:solid; border-color:#3E004A; padding: 1em;"> 
+  <p style="letter-spacing:2px ;font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color:#493743;font-size:18px;border:20px;border-width: 40px; border-style:solid; border-color:#5C038C; padding: 2em;">
+    <b>Specially for the customers who love the color purple. </b><br></br>
+    <b style="font-size:23">This type is limited in our brownies world. </b><br></br>
+  <b>IF YOU WANT TO ORDER IT, SHOULD INFORM US ATLEAST <span style="color:#D43790">TWO WEEKS BEFORE</span></b><br></br></p>
+  <b><h3 style="color:#44110E;font-family:Garamond">BLACKBERRY CHEESE CAKE BROWNIES CAN BE CUSTOMERIZED ACCORDING TO YOUR DESIGNS.</h3>
+  <ul style="color:#201A1E;font:15;font-family:Georgia, 'Times New Roman', Times, serif"><b>
+  </ul>
+  <h4>See the prices with sizes availbale</h4><button class="button button1" onClick="location.href='prices.html'">Prices</button></img>
+  <br><br><br><br><br><br><br>
+
+  <!--menue 4-->
+  <h4 style="font-size:30px"><ul style="list-style-type:square">
+    <li>Peanut Butter Brownies</li></h4> 
+  <img src="peanutbutterbrownie.jpg" ALIGN="left"HSPACE="40"class="text1" alt="one" width="370" height="460" style="border-radius:20px;border-width:100px; border:50px;border-style:solid; border-color:#F7AE53; padding: 1em;"> 
+  <p style="letter-spacing:2px ;font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color:#593F3B;font-size:18px;border:20px;border-width: 40px; border-style:solid; border-color:#593F3B; padding: 2em;">
+    <b>For absolutely peanut butter lovelies . it gives cheese cake taste with peanut butter. </b><br></br>
+    <b>Extremely delicious and healthy. </b><br></br></p><br></br>
+  <b><h3 style="color:#44110E;font-family:Garamond">PEANUT BUTTER BROWNIES CAN BE CUSTOMERIZED ACCORDING TO YOUR DESIGNS.</h3>
+  <ul style="color:#201A1E;font:15;font-family:Georgia, 'Times New Roman', Times, serif"><b>
+  </ul><h4>See the prices with sizes availbale</h4><button class="button button1" onClick="location.href='prices.html'">Prices</button></img>  
+  <br></br>  <br></br><br>  
+
+<!--menue 5-->
+<h4 style="font-size:30px"><ul style="list-style-type:square">
+  <li>Caramel Brownies</li></h4> 
+<img src="caramelbrownie.jpg" ALIGN="left"HSPACE="40"class="text1" alt="one" width="370" height="460" style="border-radius:20px;border-width:100px; border:50px;border-style:solid; border-color:#493743; padding: 1em;"> 
+<p style="letter-spacing:2px ;font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color:#593F3B;font-size:18px;border:20px;border-width: 40px; border-style:solid; border-color:#F1A17E; padding: 2em;">
+  <b>For my customers who love playing around with different flavor combinations.  </b><br></br>
+  <b>You will experienced a heavenly taste because it is extremely delicious </b><br></br></p>
+<b><h3 style="color:#44110E;font-family:Garamond">PEANUT BUTTER BROWNIES CAN BE CUSTOMERIZED ACCORDING TO YOUR DESIGNS.</h3>
+<ul style="color:#201A1E;font:15;font-family:Georgia, 'Times New Roman', Times, serif"><b>
+</ul><h4>See the prices with sizes availbale</h4><button class="button button1" onClick="location.href='prices.html'">Prices</button></img>  
+<br><br><br><br><br><br><br><br>
+
+<!--menue 6-->
+<h4 style="font-size:30px"><ul style="list-style-type:square">
+  <li>Strawberry Brownies</li></h4> 
+<img src="strawberrybrownies.jpg" ALIGN="left"HSPACE="40"class="text1" alt="one" width="370" height="460" style="border-radius:20px;border-width:100px; border:50px;border-style:solid; border-color:#970C10; padding: 1em;"> 
+<p style="letter-spacing:2px ;font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color:#593F3B;font-size:18px;border:20px;border-width: 40px; border-style:solid; border-color:#F7EDD0; padding: 2em;">
+  <b> These brownies are light and crumbly, with a punch of strawberry flavour.  </b><br></br>
+  <b>There are no limits on what can be used when baking and cooking </b><br></br>
+  <b>So that we used fresh strawberries in making this kind. </b><br></br></p>
+<b><h3 style="color:#44110E;font-family:Garamond">STRABERRY BROWNIES CAN BE CUSTOMERIZED ACCORDING TO YOUR DESIGNS.</h3>
+<ul style="color:#201A1E;font:15;font-family:Georgia, 'Times New Roman', Times, serif"><b>
+</ul><h4>See the prices with sizes availbale</h4><button class="button button1" onClick="location.href='prices.html'">Prices</button></img>  
+<br></br>  <br></br> <br>
+
+<!--menue 7-->
+<h4 style="font-size:30px"><ul style="list-style-type:square">
+  <li>Coffee Brownies</li></h4> 
+<img src="coffebrownies.jpg" ALIGN="left"HSPACE="40"class="text1" alt="one" width="370" height="460" style="border-radius:20px;border-width:100px; border:50px;border-style:solid; border-color:#5D5450; padding: 1em;"> 
+<p style="letter-spacing:2px ;font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; color:#593F3B;font-size:18px;border:20px;border-width: 40px; border-style:solid; border-color:#6B3A2C; padding: 2em;">
+  <b> You will Love this taste for sure. You can also experience cinnamon in chocolate.</b><br></br>
+  <b>For the customers who don’t like coffee taste,  </b><br></br>
+  <b>“ YOU SHOULD TASTE THIS LOVELIES!”.</b><br></br></p>
+<b><h3 style="color:#44110E;font-family:Garamond">COFFEE BROWNIES CAN BE CUSTOMERIZED ACCORDING TO YOUR DESIGNS.</h3>
+<ul style="color:#201A1E;font:15;font-family:Georgia, 'Times New Roman', Times, serif"><b>
+</ul><h4>See the prices with sizes availbale</h4><button class="button button1" onClick="location.href='prices.html'">Prices</button></img>  
+<br></br>  <br></br> <br>
+
+<!--menue 8-->
+<h4 style="font-size:30px"><ul style="list-style-type:square">
+  <li>Other Brownie Products</li></h4> 
+  <div class="row">
+    <div class="column">
+      <div class="card">
+        <img src="browniecake.jpg"alt="browniecake" style="width:300px;height:400px; border-radius:15px;border-width:30px; border:20px;border-style:solid; border-color:#291211; padding: 1em;">
+        <div class="container">
+          <h2>Brownie Cake</h2>
+          <p class="title">A delicious and decadent Brownie Cake made from scratch</p>
+          <p>This moist and tender cake is made using real chocolate and meringue-like crust.<br><br><span style="color:#5C3D2E">BROWNIE CAKE CAN BE CUSTOMERIZED ACCORDING TO YOUR DESIGNS.</span></p>
+          <h4>See the prices with sizes availbale</h4><button class="button button1" onClick="location.href='prices.html'">Prices</button>
+        </div>
+      </div>
+    </div>
+  
+    <div class="column">
+      <div class="card">
+        <img src="browniepitza.jpg"alt="browniepizza" style="width:300px;height:400px; border-radius:15px;border-width:30px; border:20px;border-style:solid; border-color:#291211; padding: 1em;">
+        <div class="container">
+          <h2>Brownie Pizza</h2>
+          <p class="title">A delicious and decadent Brownie pizza made for young</p>
+          <p>Busy parents love this super simple, candy-topped brownie. It's guaranteed to be a hit at any party for kids.<br><br><span style="color:#5C3D2E">BROWNIE PIZZA CAN BE CUSTOMERIZED ACCORDING TO YOUR DESIGNS.</span></p>
+          <h4>See the prices with sizes availbale</h4><button class="button button1" onClick="location.href='prices.html'">Prices</button>
+        </div>
+      </div>
+    </div>
+    
+    <div class="column">
+      <div class="card">
+        <img src="browniecookies.jpg"alt="browniecookie" style="width:300px;height:400px; border-radius:15px;border-width:30px; border:20px;border-style:solid; border-color:#291211; padding: 1em;">
+        <div class="container">
+          <h2>Brownie buiscuit</h2>
+          <p class="title">A delicious and decadent Browni cookies are speciallymade of little kids and can also tasted by adlts.</p>
+          <p>Busy parents love this super simple, candy-topped brownie. It's guaranteed to be a hit at any party for kids.<br><br><span style="color:#5C3D2E">BROWNIE PIZZA CAN BE CUSTOMERIZED ACCORDING TO YOUR DESIGNS.</p>
+            <h4>See the prices with sizes availbale</h4><button class="button button1" onClick="location.href='prices.html'">Prices</button>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+
+  <!-- Contact Section -->
+  <div class="aboutcontact" id="contact">
+    
+    <form action="form1.php" method="POST" class="decor" align:"middle" name="form1">
+      <div class="form-left-decoration"id="contact"></div>
+      <div class="form-right-decoration"></div>
+      <div class="circle"></div>
+      <div class="form-inner">
+        <h1>Contact us</h1>
+        <input type="text" placeholder="Name of the customer" name="name"style="color:#F3E1C0">
+        <input style="color:#F3E1C0" type="text" placeholder="Home address " name="address">
+        <input style="color:#F3E1C0" type="tel" placeholder="telephone" name="tele">
+        <input  style="color:#F3E1C0"type="text" placeholder="Item/s you want to purchase " name="num" >
+        <p style="color:black;">--Please note the day which you want the order to be done--</p>
+        <input  type="date" placeholder="purchasing date" style="color: #E9DAC4;" name="when">
+        <textarea style="color:#F3E1C0" placeholder="Message..." rows="5" name="message"></textarea>
+        <button type="submit" href="/">Submit</button>
+        <p style="color:black;">--Scroll down below ,click whatsapp and inform us in there too--</p>
+      </div>
+    </form>
+
+    
+  
+  <br><br>
+<!-- End page content -->
+
+<div class="icon-bar">
+  <a class="active" href="https://www.facebook.com/brownies.rasna/"><i class="fa fa-facebook"></i></a> 
+  <a class="active" href="google.html"><i class="fa fa-google"></i></a> 
+  <a class="active" href="https://www.instagram.com/bownies_world/"><i class="fa fa-instagram"></i></a> 
+  <a class="active" href="whatsapp.html"><i class="fa fa-whatsapp"></i></a>
+  
+</div>
+<br><br><br><br><br><br>
+<div class="card">
+  <img src="morksha.png" alt="John" style="width:100%">
+  <h1>Designed By:</h1>
+  <p class="title">Morksha Rathnayake</p>
+  <p>Computer Engineering</p>
+  <p>Genreral Sir John Kotelawala Defence University</p>
+  <div style="margin: 24px 0;">
+    
+    <a href="https://www.instagram.com/_morkz_98/"><i class="fa fa-instagram"></i></a>  
+    <a href="https://www.linkedin.com/in/morksha-rathnayake-a938191b9/"><i class="fa fa-linkedin"></i></a>  
+    <a href="https://www.facebook.com/timo.morkz"><i class="fa fa-facebook"></i></a> 
+  </div>
+  
+</div>
+
+</body>
+</html>
